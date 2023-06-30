2d73fd3389931a6900ea9bab2307edea52e0353d
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-30 09:52:58 -0700

    update baru

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 const express = require('express');
-
 const usersRoute = require('./routes/users');
 const middlewareLogRequest = require('./middleware/logs'); 
 
-const app = express();
+const app = express(); 
 
 app.use(middlewareLogRequest);
 
 app.use('/users', usersRoute); 
-
-app.get("/hello", (req, res, next) => {
-    res.send('GET Hellooooo');
-});
-
-app.post("/hello", (req, res, next) => {
-    hehe = req.check_req; 
-    console.log();  
-    res.send(hehe);
-});
 
 app.listen(5002, () => {
     console.log('Server is running on Port (5002)');
