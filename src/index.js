474bb4a92ab68d59e45d0e9bcf62c2fc899993c9
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-30 05:24:23 -0700

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(middlewareLogRequest);
 app.use('/users', usersRoute); 
 
 app.get("/hello", (req, res, next) => {
-    res.send('GET Hellooso');
+    res.send('GET Helloo');
 });
 
 app.post("/hello", (req, res, next) => {
